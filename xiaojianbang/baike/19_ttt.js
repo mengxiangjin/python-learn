f384a608e2f96bd41d0d071251712d96a4c38c8c
unknown <1435145737@qq.com>
2025-07-14 08:48:03 +0800

    test

---
+++ xiaojianbang/baike/19_ttt.js
@@ -0,0 +1,38 @@
+Java.perform(function() {
+
+    var TPPlayerConfig = Java.use('com.tencent.thumbplayer.tcmedia.config.TPPlayerConfig')
+
+    TPPlayerConfig.isUserIsVip.implementation = function() {
+        var res = this.isUserIsVip()
+        console.log('isUserIsVip19',res)
+        console.log("stack: ",Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()))
+        return true
+    }
+
+    TPPlayerConfig.isUseP2P.implementation = function() {
+        var res = this.isUseP2P()
+        return true
+    }
+
+    var e = Java.use('com.tencent.thumbplayer.tcmedia.c.e')
+    e.l.implementation = function() {
+        var res = this.l()
+        console.log('resl',res)
+        return res
+    }
+    e.p.implementation = function() {
+        var res = this.p()
+        return res
+    }
+
+    e.a.overload('java.lang.String', 'java.util.Map').implementation = function(str,map) {
+        console.log('astr  ',str)
+        console.log('amap  ',map)
+        var res = this.a(str,map)
+        console.log('res a',res)
+        console.log("stack: ",Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()))
+        return res
+    }
+
+})
+
--- xuecheng/hook/10-test.py
+++ xuecheng/hook/10-test.py
@@ -2,54 +2,93 @@ import frida
 import sys
 
 #spawn程序重启即开始监听hook
+# spawn程序重启即开始监听hook
 rdev = frida.get_remote_device()
 # spawn写包名
 pid = rdev.spawn(["com.soom.live999"])
 session = rdev.attach(pid)
+
+# var
+# MissVideoPlayViewHolder = Java.use("com.miss.video.views.MissVideoPlayViewHolder");
+# // 替换类中的方法
+# MissVideoPlayViewHolder.stopPlay.implementation = function()
+# {
+#     console.log("stopPlay stopPlay：");
+# }
 
 
 # ----上面固定------以后只会动src中代码
 # src 是字符串，写js代码
 scr = """
 Java.perform(function () {
-    // Hook CommonHttpUtil.getConfig 方法
-    var HttpsUtils = Java.use("com.lzy.okgo.https.HttpsUtils");
-    
-    // 拦截 getConfig 方法
-    HttpsUtils.getSslSocketFactoryBase.implementation = function(q,p,r,t) {
-        console.log("getSslSocketFactoryBase q ",q);
-        console.log("getSslSocketFactoryBase p ",p);
-        console.log("getSslSocketFactoryBase r ",r);
-        console.log("getSslSocketFactoryBase t ",t);
-        return this.getSslSocketFactoryBase(q,p,r,t);
-    };
-    
-    var SSLContext = Java.use("javax.net.ssl.SSLContext");
-    
-    // 拦截 getConfig 方法
-    SSLContext.getInstance.overload("java.lang.String").implementation = function(protocol) {
-        console.log("enableCustomTrustManager",protocol);
-        console.log(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
-        console.log("------------------------------------------")
-        return this.getInstance(protocol);
-    };
-    
-    SSLContext.init.implementation = function(a,b,c) {
-        console.log("init a ",a)
-        console.log("init b ",b)
-        if(b != null && b.length > 0) {
-            console.log("TrustManager class: ",b[0]);
-        }
-        console.log("init c ",c)
-        this.init(a,b,c)
+    var VideosDetailsBean = Java.use("com.miss.common.bean.VideosDetailsBean");
+   //替换类中的方法
+    VideosDetailsBean.getVipFree.implementation = function(){
+        console.log("this.vipFree",this.vipFree);
+        return this.getVipFree();
     }
     
-
+    
+     var VideoMissPlayActivity = Java.use("com.miss.video.activity.VideoMissPlayActivity");
+   //替换类中的方法
+    VideoMissPlayActivity.forward.overload('android.content.Context','java.lang.String').implementation = function(ctx,p){
+        console.log("forward");
+        this.forward(ctx,p);
+    }
+ 
+     VideoMissPlayActivity.forwardSingle.implementation = function(ctx,bean){
+        console.log("getId",bean.getId());
+        console.log("getUid",bean.getUid());
+         console.log("getTitle",bean.getTitle());
+        console.log("getSummary",bean.getSummary());
+        console.log("getThumb",bean.getThumb());
+        console.log("getThumbs",bean.getThumbs());
+        console.log("getTrialUrl",bean.getTrialUrl());
+        console.log("getTrialSeconds",bean.getTrialSeconds());
+        console.log("getTrialSecondsLabel",bean.getTrialSecondsLabel());
+        console.log("getUrl",bean.getUrl());
+        console.log("getUrlW",bean.getUrlW());
+        console.log("getStatus",bean.getStatus());
+        console.log("getStatusLabel",bean.getStatusLabel());
+        console.log("getSeconds",bean.getSeconds());
+        console.log("getSecondsLabel",bean.getSecondsLabel());
+        console.log("getAddTime",bean.getAddTime());
+        console.log("getAddTimeLabel",bean.getAddTimeLabel());
+        console.log("getLat",bean.getLat());
+        console.log("getLng",bean.getLng());
+        console.log("getCity",bean.getCity());
+        console.log("getOrientation",bean.getOrientation());
+        console.log("getWeight",bean.getWeight());
+        console.log("getIsTop",bean.getIsTop());
+        console.log("getPrice",bean.getPrice());
+        console.log("getType",bean.getType());
+        console.log("getCollected",bean.getCollected());
+        console.log("getCollectNum",bean.getCollectNum());
+        console.log("getViewNum",bean.getViewNum());
+        console.log("getWatches",bean.getWatches());
+        console.log("getLikeNum",bean.getLikeNum());
+        console.log("getDislikes",bean.getDislikes());
+        console.log("getShareNum",bean.getShareNum());
+        console.log("getDeFriends",bean.getDeFriends());
+        console.log("getCommentNum",bean.getCommentNum());
+        console.log("getSales",bean.getSales());
+        console.log("getSalesAmount",bean.getSalesAmount());
+        console.log("getSalesAmountLabel",bean.getSalesAmountLabel());
+        console.log("getPurchased",bean.getPurchased());
+        console.log("getLiked",bean.getLiked());
+        console.log("getDisliked",bean.getDisliked());
+        console.log("getFollowed",bean.getFollowed());
+        console.log("getNickName",bean.getNickName());
+        console.log("getAvatar",bean.getAvatar());
+        console.log("getIsBuy",bean.getIsBuy());
+        console.log("getPriceLabel",bean.getPriceLabel());
+        console.log("getVipFree",bean.getVipFree());
+        console.log("getUserBean",bean.getUserBean());
+        this.forwardSingle(ctx,bean);
+    }
 
 });
 """
-
-
 
 script = session.create_script(scr)
 
@@ -62,34 +101,4 @@ script.on("message", on_message)
 script.load()
 rdev.resume(pid)
 sys.stdin.read()
-
-# var
-# SimpleX509TrustManager = Java.use("com.mob.tools.network.NetworkHelper$SimpleX509TrustManager");
-# // 拦截
-# getConfig
-# 方法
-# SimpleX509TrustManager.checkServerTrusted.implementation = function(q, p)
-# {
-#     console.log("SimpleX509TrustManager");
-# this.checkServerTrusted(q, p);
-# };
-#
-# SimpleX509TrustManager.$init.implementation = function(keystore)
-# {
-#     console.log("SimpleX509TrustManager$init");
-# this.$init(keystore)
-# }
-#
-# var
-# SSLTrustManager = Java.use("cn.jiguang.net.SSLTrustManager");
-#
-# // 拦截
-# getConfig
-# 方法
-# SSLTrustManager.$init.implementation = function(str)
-# {
-#     console.log("SSLTrustManager", str);
-# this.$init(str)
-# };
-
 
